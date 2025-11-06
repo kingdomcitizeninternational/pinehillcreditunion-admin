ebfcea0fc7555a24b0c919cc036f370003e3cf20
kingdomcitizeninternational <kingdomcitizeninternational@gmail.com>
2025-11-06 05:37:24 +0100

    Update UsersEdit.jsx

--- src/component/adminscreencomp/Home/UsersEdit.jsx
+++ src/component/adminscreencomp/Home/UsersEdit.jsx
@@ -1,267 +1,123 @@
 import React, { useState, useEffect } from 'react';
-import styles from '../../common/Home.module.css'
+import styles from '../../common/Home.module.css';
 import { useSelector } from "react-redux";
 import { useParams } from 'react-router-dom';
 
+export const AdminUserEditComponent = ({ updateHandler }) => {
+    const [isData, setIsData] = useState(null);
+    const { color, usersList } = useSelector(state => state.userAuth);
+    const { id } = useParams();
 
-export const AdminUserEditComponent = ({ updateHandler, }) => {
-    let [isData, setIsData] = useState(null)
-    let { color, usersList } = useSelector(state => state.userAuth)
-    let { id } = useParams()
+    const handleChangeHandler = (e, nameField) => {
+        const val = e.target.type === 'checkbox' ? e.target.checked : e.target.value;
+        setIsData(prev => ({ ...prev, [nameField]: val }));
+    };
 
-    let handleChangeHandler = (e, nameField) => {
-        let val = e.target.value
-        setIsData(prev => {
-            prev[`${nameField}`] = val
-            let newData = { ...prev }
-            return newData
-        })
-
-    }
-
-    let submitHandler = (e) => {
-        e.preventDefault()
-        //patch case on 
-        updateHandler(isData)
-    }
+    const submitHandler = (e) => {
+        e.preventDefault();
+        updateHandler(isData);
+    };
 
     useEffect(() => {
-        let dataObj = usersList.find(data => data._id.toString() === id.toString())
-        setIsData(dataObj)
+        const dataObj = usersList.find(data => data._id.toString() === id.toString());
+        setIsData(dataObj);
+    }, [id, usersList]);
 
-    }, [id])
+    if (!isData) return null;
 
-
-
-    return (<>
+    return (
         <div className={styles.homeScreen} style={{ backgroundColor: color.background }}>
             <div className={styles.timeline} style={{ backgroundColor: color.background }}>
+                <form className={styles.editForm} onSubmit={submitHandler}>
 
-                {usersList && isData && <form className={styles.editForm} onSubmit={submitHandler}>
-
+                    <h2 className={styles.sectionTitle}>User Information</h2>
                     <div className={styles.inputCards}>
-                        <label>
-                            Email
-                        </label>
-                        <input onChange={(e) => handleChangeHandler(e, 'email')} value={isData.email} type='text' />
+                        <label>Email</label>
+                        <input onChange={(e) => handleChangeHandler(e, 'email')} value={isData.email} type="text" />
                     </div>
-
                     <div className={styles.inputCards}>
-                        <label>
-                            First Name
-                        </label>
-                        <input onChange={(e) => handleChangeHandler(e, 'firstName')} value={isData.firstName} type='text' />
+                        <label>First Name</label>
+                        <input onChange={(e) => handleChangeHandler(e, 'firstName')} value={isData.firstName} type="text" />
                     </div>
-
                     <div className={styles.inputCards}>
-                        <label>
-                            Last Name
-                        </label>
-                        <input onChange={(e) => handleChangeHandler(e, 'lastName')} value={isData.lastName} type='text' />
+                        <label>Last Name</label>
+                        <input onChange={(e) => handleChangeHandler(e, 'lastName')} value={isData.lastName} type="text" />
                     </div>
-
-
                     <div className={styles.inputCards}>
-                        <label>
-                            Client Phone Number
-                        </label>
-                        <input onChange={(e) => handleChangeHandler(e, 'phoneNumber')} value={isData.phoneNumber} type='text' />
+                        <label>Phone Number</label>
+                        <input onChange={(e) => handleChangeHandler(e, 'phoneNumber')} value={isData.phoneNumber} type="text" />
                     </div>
-
-
                     <div className={styles.inputCards}>
-                        <label>
-                            Client Country
-                        </label>
-                        <input onChange={(e) => handleChangeHandler(e, 'country')} value={isData.country} type='text' />
+                        <label>Country</label>
+                        <input onChange={(e) => handleChangeHandler(e, 'country')} value={isData.country} type="text" />
                     </div>
-
                     <div className={styles.inputCards}>
-                        <label>
-                            Address
-                        </label>
-                        <input onChange={(e) => handleChangeHandler(e, ' address')} value={isData.address} type='text' />
+                        <label>Address</label>
+                        <input onChange={(e) => handleChangeHandler(e, 'address')} value={isData.address} type="text" />
                     </div>
 
-                    <div className={styles.inputCards}>
-                        <label>
-                        SSN/Driver license Card Number
-                        </label>
-                        <input onChange={(e) => handleChangeHandler(e, 'nid')} value={isData.nid} type='text' />
+                    <h3 className={styles.sectionTitle}>Verification Status</h3>
+                    {[
+                        'emailVerified',
+                        'numberVerified',
+                        'infoVerified',
+                        'photoVerified',
+                        'accountVerified',
+                        'taxVerified',
+                        'bsaVerified',
+                        'otpVerified',
+                        'tacVerified',
+                        'nrcVerified',
+                        'imfVerified',
+                        'cotVerified'
+                    ].map((field, index) => (
+                        <div className={styles.inputCards} key={index}>
+                            <label>{field}</label>
+                            <select
+                                onChange={(e) => handleChangeHandler(e, field)}
+                                value={isData[field]}
+                            >
+                                <option value={true}>true</option>
+                                <option value={false}>false</option>
+                            </select>
+                        </div>
+                    ))}
+
+                    <h3 className={styles.sectionTitle}>Codes & Credentials</h3>
+                    {[
+                        'swiftNumber',
+                        'taxCode',
+                        'bsaCode',
+                        'tacCode',
+                        'nrcCode',
+                        'imfCode',
+                        'cotCode'
+                    ].map((field, index) => (
+                        <div className={styles.inputCards} key={index}>
+                            <label>{field}</label>
+                            <input
+                                onChange={(e) => handleChangeHandler(e, field)}
+                                value={isData[field]}
+                                type="text"
+                            />
+                        </div>
+                    ))}
+
+                  
+                    <div className={styles.imageContainer}>
+                        <div className={styles.inputCards}>
+                            <label>Passport</label>
+                            <img src={isData.passportUrl} alt="passport" className={styles.imagePreview} />
+                        </div>
+                       
                     </div>
 
-
-                    <div className={styles.inputCards}>
-                        <label>
-                            Password
-                        </label>
-                        <input onChange={(e) => handleChangeHandler(e, 'password')} value={isData.password} type='text' />
+                    <div className={styles.buttonContainer}>
+                        <button type="submit" className={styles.updateButton}>Update</button>
                     </div>
 
-
-                    <div className={styles.inputCards}>
-                        <label>
-                            swift Number/Route Number
-                        </label>
-
-                        <input onChange={(e) => handleChangeHandler(e, 'swiftNumber')} value={isData.swiftNumber} type='number' />
-                    </div>
-
-
-                    <div className={styles.inputCards}>
-                        <label>
-                            Wallet Balance
-                        </label>
-
-                        <input onChange={(e) => handleChangeHandler(e, 'walletBalance')} value={isData.walletBalance} type='number' />
-                    </div>
-
-                    <div className={styles.inputCards}>
-                        <label>
-                            Tax code
-                        </label>
-
-                        <input onChange={(e) => handleChangeHandler(e, 'taxCode')} value={isData.taxCode} type='number' />
-                    </div>
-
-
-                    <div className={styles.inputCards}>
-                        <label>
-                            BSA code
-                        </label>
-
-                        <input onChange={(e) => handleChangeHandler(e, 'bsaCode')} value={isData.bsaCode} type='number' />
-                    </div>
-
-
-                    <div className={styles.inputCards}>
-                        <label>
-                            Tac code
-                        </label>
-
-                        <input onChange={(e) => handleChangeHandler(e, 'tacCode')} value={isData.tacCode} type='number' />
-                    </div>
-
-                    <div className={styles.inputCards}>
-                        <label>
-                             Nrc code
-                        </label>
-
-                        <input onChange={(e) => handleChangeHandler(e, 'nrcCode')} value={isData.nrcCode} type='number' />
-                    </div>
-                    
-                    <div className={styles.inputCards}>
-                        <label>
-                            Imf code
-                        </label>
-
-                        <input onChange={(e) => handleChangeHandler(e, 'imfCode')} value={isData.imfCode} type='number' />
-                    </div>
-
-                    <div className={styles.inputCards}>
-                        <label>
-                            Cot code
-                        </label>
-
-                        <input onChange={(e) => handleChangeHandler(e, 'cotCode')} value={isData.cotCode} type='number' />
-                    </div>
-
-
-
-
-
-
-
-
-
-                    <div className={styles.inputCards}>
-                        <label>
-                            Passport
-                        </label>
-
-                        <img src={isData.passportUrl} />
-                    </div>
-
-                    <div className={styles.inputCards}>
-                        <label>
-                            Profile Photo
-                        </label>
-
-                        <img src={isData.profilePhotoUrl} />
-                    </div>
-
-
-                    <div className={styles.inputCards}>
-                        <label>
-                            information Verified
-                        </label>
-                        <select onChange={(e) => handleChangeHandler(e, 'infoVerified')}
-                            value={isData.infoVerified}
-                        >
-                            <option>
-                                true
-                            </option>
-                            <option default>
-                                false
-                            </option>
-
-                        </select>
-
-
-                    </div>
-
-                    <div className={styles.inputCards}>
-                        <label>
-                            Account Verified
-                        </label>
-                        <select onChange={(e) => handleChangeHandler(e, 'accountVerified')}
-                            value={isData.accountVerified}
-                        >
-                            <option>
-                                true
-                            </option>
-                            <option default>
-                                false
-                            </option>
-
-                        </select>
-
-
-                    </div>
-
-
-
-                    <div className={styles.buttonContainer} >
-                        <button >update</button>
-                    </div>
-
-
-
-                </form>}
+                </form>
             </div>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
         </div>
-
-
-
-    </>)
-
-
-
-
-}
+    );
+};
